f416021ef92659725cd91a1fe7347b58f6833cf8
vidaleishon <jo.vidalei@gmail.com>
2022-11-28 17:11:49 -0300

    estructura de html y comiendo de js

---
+++ main.js
@@ -0,0 +1,13 @@
+let nombre
+let apellido
+let edad
+
+nombre = "Jordi" 
+apellido = "Vidal" 
+edad = 22
+
+let nombreIngresado = prompt ("Cómo se llama?");
+
+let respuesta = "Saludos" + " " + nombreIngresado;
+alert (respuesta)
+
