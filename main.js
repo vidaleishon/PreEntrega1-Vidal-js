20427c8eda5ad36b475ee4dc2f3210ef551a8642
vidaleishon <jo.vidalei@gmail.com>
2022-12-05 20:16:06 -0300

    Ajustes js

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
 //Ingrese un nombre
 
-let nombreIngresado = prompt ("¿Cuál es su nombre?");
+let nombreIngresado = prompt("¿Cuál es su nombre?");
 
-if (nombreIngresado !="") {
-    alert ("Saludos, " + nombreIngresado)
+if (nombreIngresado != "") {
+    alert("Saludos, " + nombreIngresado)
 }
 else {
-    alert ("Por favor, la próxima ingrese su nombre.") 
-    
+    alert("Por favor, la próxima vez ingrese su nombre.")
+
 }
 
 //Ingrese un número
 
-let ingresarNumero = prompt ("Ingrese un número del 1 al 10")
+let ingresarNumero = prompt("Por favor, ingrese un número del 1 al 10")
+let i = ingresarNumero
 
-for (let i = 0; i < 10 ; i++) {
+for (let i = 1; i < 10; i++) {
     if (i == ingresarNumero) {
+        alert("Usted ha elegido el número " + i + "!")
         break;
     }
-     alert (i + 1)
 }
+
+
 switch (ingresarNumero) {
-    case "1": break;
-    case "2": break;
-    case "3": break;
-    case "4": break;
-    case "5": break;
-    case "6": break;
-    case "7": break;
-    case "8": break;
-    case "9": break;
-    case "10": break;
-    case ">10": alert ("Dato muy grande")
-    default: alert ("Dato incorrecto.")
-    
+    case "1":
+        break;
+    case "2":
+        break;
+    case "3":
+        break;
+    case "4":
+        break;
+    case "5":
+        break;
+    case "6":
+        break;
+    case "7":
+        break;
+    case "8":
+        break;
+    case "9":
+        break;
+    case "10":
+        break;
+    default: alert("Dato incorrecto.")
+
 }
