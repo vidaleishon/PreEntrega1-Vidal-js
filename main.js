758b7961b07d6075dea1322fba377a670010b4b6
vidaleishon <jo.vidalei@gmail.com>
2022-12-13 11:23:06 -0300

    ligero cambio de opciones js

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-let consultaEntrada = prompt("Buenas! ¿Eres tutor o invitado?").toLowerCase()
+let consultaEntrada = prompt("Buenas! ¿Eres tutor o invitado?").toLowerCase();
 
 if (consultaEntrada == "tutor") {
-    alert ("Bienvenido, tutor!")
+  alert("Bienvenido, tutor!");
 } else if (consultaEntrada == "invitado") {
-    alert ("Bienvenido!")
+  alert("Bienvenido!");
 }
 
-while (consultaEntrada != "tutor" && consultaEntrada != "invitado") {      
-    alert ("Por favor, especifique.");
-    consultaEntrada = prompt ("¿Eres tutor o invitado?").toLowerCase();
-    if (consultaEntrada == "tutor") {
-        alert ("Bienvenido, tutor!")
-    } else if (consultaEntrada == "invitado") {
-        alert ("Bienvenido!")
-    }
+while (consultaEntrada != "tutor" && consultaEntrada != "invitado") {
+  alert("Por favor, especifique.");
+  consultaEntrada = prompt("¿Eres tutor o invitado?").toLowerCase();
+  if (consultaEntrada == "tutor") {
+    alert("Bienvenido, tutor!");
+  } else if (consultaEntrada == "invitado") {
+    alert("Bienvenido!");
+  }
 }
 
-let consultaReserva = prompt("¿Desea reservar?").toLowerCase()
+let consultaReserva = prompt("¿Desea reservar?").toLowerCase();
 if (consultaReserva == "si") {
-    alert ("Perfecto!")
-} else if (consultaReserva == "no"){
-    alert ("Qué lastima!")
+  alert("Perfecto!");
+} else if (consultaReserva == "no") {
+  alert("Qué lastima!");
 }
 
-while ( consultaReserva !="si" && consultaReserva != "no") {
-      alert ("Por favor, especifique si o no.")
-      consultaReserva = prompt("¿Desea reservar?").toLowerCase()
-      if (consultaReserva == "si") {
-        alert ("Perfecto!")
-    } else if (consultaReserva == "no"){
-        alert ("Qué lastima!")
-    }
+while (consultaReserva != "si" && consultaReserva != "no") {
+  alert("Por favor, especifique si o no.");
+  consultaReserva = prompt("¿Desea reservar?").toLowerCase();
+  if (consultaReserva == "si") {
+    alert("Perfecto!");
+  } else if (consultaReserva == "no") {
+    alert("Qué lastima!");
+  }
 }
 
-while (consultaReserva == "si") { 
-    let cantidadFechas = parseInt(prompt("Cuántas noches necesita?"))
-    alert ("El total es $" + 6500*cantidadFechas)
-    break;
-} 
+while (consultaReserva == "si") {
+  let cantidadFechas = parseInt(prompt("Cuántas noches necesita?"));
+  alert("El total es $" + 6500 * cantidadFechas);
+  break;
+}
