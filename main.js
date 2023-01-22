46e16b810113ed7cf8493bbe95e09ee1ce11455d
vidaleishon <jo.vidalei@gmail.com>
2023-01-21 23:29:20 -0300

    ajustes de precio y movimientos de css

--- main.js
+++ main.js
@@ -1,51 +1,36 @@
-let consultaEntrada = prompt("Buenas! ¿Eres tutor o invitado?").toLowerCase();
-
-if (consultaEntrada == "tutor") {
-  alert("Bienvenido, tutor!");
-} else if (consultaEntrada == "invitado") {
-  alert("Bienvenido!");
-}
-
-while (consultaEntrada != "tutor" && consultaEntrada != "invitado") {
-  alert("Por favor, especifique.");
-  consultaEntrada = prompt("¿Eres tutor o invitado?").toLowerCase();
-  if (consultaEntrada == "tutor") {
-    alert("Bienvenido, tutor!");
-  } else if (consultaEntrada == "invitado") {
-    alert("Bienvenido!");
+class Reserva {
+  constructor(nombreReserva, emailReserva, fechaInicioReserva, fechaFinReserva, datosExtra) {
+    this.nombre = nombreReserva;
+    this.email = emailReserva;
+    this.fechaLlegada = fechaInicioReserva;
+    this.fechaSalida = fechaFinReserva;
+    this.datosExtra = datosExtra;
   }
 }
+function reserva() {
+  let nombreReserva = document.getElementById("nombreyapellido").value;
+  let emailReserva = document.getElementById("email").value;
+  let fechaInicioReserva = document.getElementById("fechallegada").value;
+  let fechaFinReserva = document.getElementById("fechasalida").value;
+  let datosExtra = document.getElementById("datosextra").value;
+  var fechaLlegada = new Date(fechaInicioReserva).getTime();
+  var fechaSalida = new Date(fechaFinReserva).getTime();
+  var diff = fechaSalida - fechaLlegada;
+  var cantidadDias = diff / (1000 * 60 * 60 * 24);
+  var precioTotal = cantidadDias * 13500;
 
-let consultaReserva = prompt("¿Desea reservar?").toLowerCase();
-if (consultaReserva == "si") {
-  alert("Perfecto!");
-} else if (consultaReserva == "no") {
-  alert("Qué lastima!");
-}
+  const nuevaReserva = new Reserva(nombreReserva, emailReserva, fechaInicioReserva, fechaFinReserva, datosExtra)
 
-while (consultaReserva != "si" && consultaReserva != "no") {
-  alert("Por favor, especifique si o no.");
-  consultaReserva = prompt("¿Desea reservar?").toLowerCase();
-  if (consultaReserva == "si") {
-    alert("Perfecto!");
-  } else if (consultaReserva == "no") {
-    alert("Qué lastima!");
+  console.log(nuevaReserva)
+
+  if (cantidadDias >= 7 && cantidadDias < 16) {
+    console.log("Debido a la cantidad de días de su posible estadía, se le ofrecerá un descuento del 5%. El precio final de su reserva es de ARS$ " + (precioTotal * 0.95))
+  } else if (cantidadDias >= 16 && cantidadDias < 30) {
+    console.log("Debido a la cantidad de días de su posible estadía, se le ofrecerá un descuento del 8%. El precio final de su reserva es de ARS$ " + (precioTotal * 0.92))
+  } else if (cantidadDias > 30) {
+    console.log("Debido a la cantidad de días de su posible estadía, se le ofrecerá un descuento del 12%. El precio final de su reserva es de ARS$ " + (precioTotal * 0.88))
+  } else {
+    console.log("Su reserva es de " + cantidadDias + " días. El precio final de su reserva es de $" + precioTotal)
   }
+
 }
-
-while (consultaReserva == "si") {
-  let cantidadFechas = parseInt(prompt("Cuántas noches necesita?"));
-  alert("El total es $" + 13500 * cantidadFechas);
-  break;
-} 
-
-function handleSubmit(e) {
-    e.preventDefault();
-    let fechaLlegada = element.getElementById("fechallegada").value
-    let fechaSalida = element.getElementById("fechasalida").value
-    var fechaInicio = new Date(fechaLlegada).getTime();
-    var fechaFinal = new Date(fechaSalida).getTime();
-    var diff = fechaFinal - fechaInicio;
-
-    console.log(diff/(1000*60*60*24));
-  }
