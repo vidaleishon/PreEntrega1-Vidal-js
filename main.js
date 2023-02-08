af20cc9cc0e5cf78efef5ee3dde2be1d7a48125e
vidaleishon <jo.vidalei@gmail.com>
2023-02-08 20:21:25 -0300

    arreglos

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 class Reserva {
   constructor(nombreReserva, emailReserva, fechaInicioReserva, fechaFinReserva, datosExtra) {
     this.nombre = nombreReserva;
@@ -7,6 +8,9 @@ class Reserva {
     this.datosExtra = datosExtra;
   }
 }
+
+const datosReserva = []
+
 function reserva() {
   let nombreReserva = document.getElementById("nombreyapellido").value;
   let emailReserva = document.getElementById("email").value;
@@ -18,11 +22,12 @@ function reserva() {
   var diff = fechaSalida - fechaLlegada;
   var cantidadDias = diff / (1000 * 60 * 60 * 24);
   var precioTotal = cantidadDias * 13500;
+  
+  const nuevaReserva = new Reserva(nombreReserva, emailReserva, fechaInicioReserva, fechaFinReserva, datosExtra);
 
-  const nuevaReserva = new Reserva(nombreReserva, emailReserva, fechaInicioReserva, fechaFinReserva, datosExtra)
-
+  
   console.log(nuevaReserva)
-
+  
   let descuento = 1;
   if (cantidadDias >= 7 && cantidadDias < 16) {
     descuento = 0.95;
@@ -36,26 +41,31 @@ function reserva() {
   } else {
     console.log("Su reserva es de " + cantidadDias + " noches. El precio final de su reserva es de $" + precioTotal)
   }
-
+  
   let precioFinal = precioTotal * descuento
-
+  
   let nodo = document.createElement("div");
   nodo.innerHTML = `<h3>Su reserva:</h3>
-   <h5> Nombre: </h5> <p>${nombreReserva}</p> 
-   <h5> Email: </h5><p>${emailReserva}</p>   
-   <h5> Fecha de Llegada: </h5><p>${fechaInicioReserva}</p> 
-   <h5> Fecha de Salida: </h5><p>${fechaFinReserva}</p> 
-   <h5> Datos a tener en cuenta: </h5><p>${datosExtra}</p> 
-   <p> Realizada desde el día ${fechaInicioReserva} hasta el día ${fechaFinReserva}. Por un total de ${cantidadDias} noches, el precio total es de <strong>ARS$${precioFinal}</strong>. </p>`
-
+  <h5> Nombre: </h5> <p>${nombreReserva}</p> 
+  <h5> Email: </h5><p>${emailReserva}</p>   
+  <h5> Fecha de Llegada: </h5><p>${fechaInicioReserva}</p> 
+  <h5> Fecha de Salida: </h5><p>${fechaFinReserva}</p> 
+  <h5> Datos a tener en cuenta: </h5><p>${datosExtra}</p> 
+  <p> Realizada desde el día ${fechaInicioReserva} hasta el día ${fechaFinReserva}. Por un total de ${cantidadDias} noches, el precio total es de <strong>ARS$${precioFinal}</strong>. </p>`
+  
   document.getElementById("respuestaconsulta").appendChild(nodo);
-
-
+  
+  
+  datosReserva.push(nuevaReserva);
+  return datosReserva;
+  
 }
 
 const enviarDato = document.getElementById("botonenviar");
 
-enviarDato.onclick = (e) => {
+const formulario = document.querySelector("#formulario_reserva");
+
+formulario.onsubmit = (e) => {
   e.preventDefault();
   reserva(e);
   
